4a6956b8d1e0d41c9953761ebe42a6c36945f970
Jules <Julian@nassberger.at>
2025-09-23 15:01:43 +0200

    inital commit

---
+++ src/main/java/at/nassberger/demo/HomeController.java
@@ -0,0 +1,35 @@
+package at.nassberger.demo;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    Logger log=Logger.getLogger("at.nassberger.demo.MainController");
+
+    @GetMapping("/")
+    public String form() {
+        log.log(Level.INFO, "calling form() method");
+        String html = "<html><head>Einfaches POST Formular</head>"
+            + "<body><h1>Mein Formular</h1><hr>"
+            + "<form action='./save' method='POST'>"
+            + "Vorname: <input type='text' name='firstname'></br>"
+            + "Nachname:<input type='text' name='lastname'></br>"
+            + "<input type='submit' value='senden'>"
+            + "</form>"
+            + "</body></html>";
+        return html;
+    }
+        
+    @PostMapping("/save")
+    public String save(@ModelAttribute UserData data) {
+        log.log(Level.INFO, "calling save() method");
+        return "ok, recieved data: firstname: " + data.getFirstname() + ", lastname: " + data.getLastname();
+    }
+}
---
+++ src/main/java/at/nassberger/demo/UserData.java
@@ -0,0 +1,21 @@
+package at.nassberger.demo;
+
+public class UserData {
+
+    String firstname;
+    String lastname;
+
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    } 
+ 
+}
